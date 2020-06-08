7656ff852cdfcfa6619e124562ed46f741986d24
Ammarhr <hariry49@gmail.com>
2020-06-08 09:40:18 +0300

    validation sign up

--- models/usersmodel.js
+++ models/usersmodel.js
@@ -3,25 +3,24 @@
 const userSchema = require('./user-schama');
 
 class User {
-  constructor(userSchema) {
-    this.schama = userSchema;
-  }
-  async read(record) {
-    //         let newUser = new userSchema(record);
-    if (record) {
-      //   console.log('<-------------------------- this is the user name ------------------------------->', record)
-      let senc = await userSchema.find({ user_name: record });
-      //   console.log('<-------------------------- this is the user name ------------------------------->', senc)
-      return senc;
-
-    } else {
-      return await userSchema.find({});
+    constructor(userSchema) {
+        this.schama = userSchema;
     }
-  }
-  async create(record) {
-    let newUser = new userSchema(record);
-    return await newUser.save(record);
-  }
+    async read(record) {
+        //         let newUser = new userSchema(record);
+        if (record) {
+            //   console.log('<-------------------------- this is the user name ------------------------------->', record)
+            let senc = await userSchema.find({ user_name: record });
+            //   console.log('<-------------------------- this is the user name ------------------------------->', senc)
+            return senc;
+        } else {
+            return await userSchema.find({});
+        }
+    }
+    async create(record) {
+        let newUser = new userSchema(record);
+        return await newUser.save(record);
+    }
 }
 
 module.exports = new User();
