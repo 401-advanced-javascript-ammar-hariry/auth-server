740544e0bb42f3f5d7bb1ffe79d782be585352af
Ammarhr <hariry49@gmail.com>
2020-06-08 10:06:10 +0300

    solving issue

--- middleware/BasicAuthentication.js
+++ middleware/BasicAuthentication.js
@@ -3,23 +3,23 @@ const base64 = require('base-64');
 const users = require('../auth/user-auth');
 
 module.exports = (req, res, next) => {
-  if (!req.headers.authorization) {
-    //         console.log('=====================================================');
-    next('Invalid Login');
-  } else {
-    //         console.log('=======================here==============================', req.headers.authorization);
-    const basic = req.headers.authorization.split(' ').pop();
-    //         console.log('=======================here 2222222==============================', basic);
+    if (!req.headers.authorization) {
+        //         console.log('=====================================================');
+        next('Invalid Login');
+    } else {
+        //         console.log('=======================here==============================', req.headers.authorization);
+        const basic = req.headers.authorization.split(' ').pop();
+        //         console.log('=======================here 2222222==============================', basic);
 
-    const [user, pass] = base64.decode(basic).split(':');
-    //         console.log('=======================here 33333333==============================', [user, pass]);
-    users.authenticateBasic(user, pass)
-      .then(validator => {
-        req.token = users.getToken(validator);
-        //       console.log('=======================here 44444444444==============================', req.token);
-        next();
-      }).catch(err => {
-        next(err.message);
-      });
-  }
+        const [user, pass] = base64.decode(basic).split(':');
+        //         console.log('=======================here 33333333==============================', [user, pass]);
+        users.authenticateBasic(user, pass)
+            .then(validator => {
+                req.token = users.getToken(validator);
+                //       console.log('=======================here 44444444444==============================', req.token);
+                next();
+            }).catch(err => {
+                next('you must sign up');
+            });
+    }
 };
